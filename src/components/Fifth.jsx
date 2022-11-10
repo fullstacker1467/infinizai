330d10d1a593f17f6c8ee1e4677235e2c8e75b97
senior <suhrobrahmatullayev973132@gmail.com>
2022-11-10 17:56:38 +0500

    fdgirjgoeruhgrieuhg

--- src/components/Fifth.jsx
+++ src/components/Fifth.jsx
@@ -5,7 +5,7 @@ function Fifth() {
         <div className='fifth'>
             <div className="container">
                 <h1>Join the largest gaming community !</h1>
-                <p>lectus. Cras ultricies ligula sed magna dictum porta. Nulla quis lorem ut libero malesu ada feugiat. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo</p>
+                <p>lectus. Cras ultricies ligula sed dictum porta. Nulla quis lorem ut libero malesu ada feugiat. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo</p>
                 <form className="inp">
                 <input type="email" placeholder='Your Email Address'/>
                 <button type='submit'>Subscribe Now  <i class="fa-solid fa-arrow-right"></i></button>
